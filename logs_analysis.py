e802e70769fe50448af3adc23b2639d73d2ffabe
Ryan Karumanchery <ryankarumanchery@gmail.com>
2017-08-01 22:55:41 -0400

    query2 added

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,10 +1,7 @@
 import psycopg2
 
-# Most Popular articles, descending order
 
 # Analysis Function
-
-
 def Analysis(query):
     dbname = "news"
     database = psycopg2.connect(database=dbname)
@@ -14,10 +11,21 @@ def Analysis(query):
     print rows
     database.close()
 
-query1 = "select title, count(ip) as count from articles, log group by title order by count desc limit 3 offset 1"
 
-query2 = ""
+# Most Popular articles, descending order
+query1 ="""select title, count(*) as count from articles, log
+where log.path like concat('%', articles.slug)
+group by articles.title order by count desc limit 3"""
 
+# Most Popular article authors
+query2 = """select authors.name, count(articles.author) as count from articles,
+        log, authors where
+        log.path = concat('/article/',articles.slug)
+        and articles.author = authors.id
+        group by authors.name
+        order by count desc"""
+
+# Days where more than 1% of requests led to errors
 query3 = ""
 
 Analysis(query1)
