34f29659efee306a8ea044fb8fa8471ddbcee9bd
Ryan Karumanchery <ryankarumanchery@gmail.com>
2017-08-02 21:11:49 -0400

    Create-views moved to README.md

--- logs_analysis.py
+++ logs_analysis.py
@@ -31,30 +31,12 @@ query2 = """SELECT authors.name, COUNT(articles.author) AS count
 
 # Days WHERE more than 1% of requests led to errors
 
-# first create this view
-    """CREATE VIEW error_view AS
-    SELECT date(time), COUNT(*) AS errors
-    FROM log
-    WHERE status LIKE '%404%'
-    GROUP BY date(time)
-    ORDER BY date(time);"""
-
-"""CREATE VIEW full_view AS
-SELECT date(time), COUNT(*) AS views
-FROM log
-GROUP BY date(time)
-ORDER BY date(time) """
-
-"""CREATE VIEW error_percentage AS
-SELECT full_view.date, (100.0*error_view.errors/full_view.views) AS rate
-FROM full_view, error_view
-WHERE full_view.date = error_view.date
-ORDER BY full_view.date; """
-
 query3 = """SELECT *
     FROM error_percentage
     WHERE error_percentage.rate > 1
     ORDER BY error_percentage.rate DESC; """
-"
 
+
+Analysis(query1)
 Analysis(query2)
+Analysis(query3)
